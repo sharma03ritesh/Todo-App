fbf1004489b1b575cc5ead3325615f0698d65503
Ritesh Sharma <ritesharma@sviet.ac.in>
2026-09-21 17:51:39 +0530

    update file structure

--- src/App.js
+++ src/App.js
@@ -9,11 +9,56 @@ let password = "Test@123";
 function App() {
   
   const [Loggedin, setLoggedin] = useState(Boolean);
+  const [usernameCopied, setUsernameCopied] = useState(false);
+  const [passwordCopied, setPasswordCopied] = useState(false);
+  const handleCopy = async (textToCopy, type) => {
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+
+      if (type === "username") {
+        setUsernameCopied(true);
+
+        setTimeout(() => {
+          setUsernameCopied(false);
+        }, 2000);
+      }
+
+      if (type === "password") {
+        setPasswordCopied(true);
+
+        setTimeout(() => {
+          setPasswordCopied(false);
+        }, 2000);
+      }
+    } catch (err) {
+      console.error("Failed to copy text:", err);
+    }
+  };
+
   return (
     <>    
 
     <div className="App">
       <header className="App-header">
+          <p>
+            use username: <code>{username}</code>{" "}
+            <button
+              onClick={() => handleCopy(username, "username")}
+              className="btn btn-primary"
+            >
+              {usernameCopied ? "Copied!" : "Copy"}
+            </button>
+          </p>
+
+          <p>
+            use password: <code>{password}</code>{" "}
+            <button
+              onClick={() => handleCopy(password, "password")}
+              className="btn btn-primary"
+            >
+              {passwordCopied ? "Copied!" : "Copy"}
+            </button>
+          </p>
           {Loggedin === false ? <TodoLogin userid = {username} passwd = {password} username = "Username" password = "Password" Loggedin = {Loggedin} setLoggedin = {setLoggedin}/> : <TodoList User = {username} Loggedin = {Loggedin} setLoggedin = {setLoggedin} />}
           
       </header>
